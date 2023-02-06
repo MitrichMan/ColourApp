cb4012b4c515ab7e2ccb5fed4b916a12f21b388d
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-02-06 19:37:54 +0800

    Work with app logic

--- ColourApp/ViewController.swift
+++ ColourApp/ViewController.swift
@@ -7,13 +7,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redColorValueLabel: UILabel!
+    @IBOutlet var greenColorValueLabel: UILabel!
+    @IBOutlet var blueColorValueLabel: UILabel!
+    
+    @IBOutlet var redColorSlider: UISlider!
+    @IBOutlet var greenColorSlider: UISlider!
+    @IBOutlet var blueColorSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redColorValueLabel.text = redColorSlider.value.formatted()
+        greenColorValueLabel.text = greenColorSlider.value.formatted()
+        blueColorValueLabel.text = blueColorSlider.value.formatted()
     }
-
-
+    
+    private func changeViewColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redColorSlider.value),
+            green: CGFloat(greenColorSlider.value),
+            blue: CGFloat(blueColorSlider.value),
+            alpha: 1
+        )
+    }
+    
+    @IBAction func redColorSliderAction() {
+        redColorValueLabel.text = (round(redColorSlider.value * 100) / 100).formatted()
+        changeViewColor()
+    }
+    
+    @IBAction func greenColorSliderAction() {
+        greenColorValueLabel.text = (round(greenColorSlider.value * 100) / 100).formatted()
+        changeViewColor()
+    }
+    
+    @IBAction func blueColorSliderAction() {
+        blueColorValueLabel.text = (round(blueColorSlider.value * 100) / 100).formatted()
+        changeViewColor()
+    }
+    
 }
 
